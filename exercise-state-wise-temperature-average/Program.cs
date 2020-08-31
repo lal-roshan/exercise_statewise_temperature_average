a1266fd5f7990a4c44520d71cf96b46eea0f4e2e
Nandita Vora <Nandita.Vora@in.niit.com>
2020-08-31 22:33:49 +0530

    initial commit

---
+++ exercise-state-wise-temperature-average/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercise_state_wise_temperature_average
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*declare array for collecting temperature of cities of 3 states
+             *
+             *first state provides temperature of 3 cities
+             *second state provides temperature of 7 cities
+             *third state provides temperature of 4 cities
+            */
+
+            /*
+             * Call GetStateWiseAverage() with state position and temperature data
+             */
+
+            /*
+             * Display Average
+             */
+        }
+
+        /* 
+         * Method calculates average of temperature for the state whose position is provided
+         */
+
+        public static int GetStateWiseAverage()
+        {
+            // put here logic to calculate average
+        }
+    }
+}
---
+++ test/StateWiseTemperatureAverageTest.cs
@@ -0,0 +1,35 @@
+﻿using exercise_state_wise_temperature_average;
+using NUnit.Framework;
+using System;
+
+namespace test
+{
+    [TestFixture]
+    public class StateWiseTemperatureAverageTest
+    {
+        [TestCase(new int[] { 9, 8, 2 }, ExpectedResult = 6)]
+        [TestCase(new int[] { 9, 8, 4 }, ExpectedResult = 7)]
+
+        public int GetStateWiseAverage_Should_Return_Average(int[] values)
+        {
+            int[][] temp = new int[1][]
+                                    {
+                                        values
+                                    };
+            var result = Program.GetStateWiseAverage(0, temp);
+            return result;
+        }
+
+        [TestCase(2)]
+        [TestCase(3)]
+        public void GetStateWiseAverage_Should_Throw_Exception(int index)
+        {
+            int[][] temp = new int[][]
+                                    {
+                                        new int[]{ 9, 8, 2 }
+                                    };
+            Assert.That(() => Program.GetStateWiseAverage(2, temp), Throws.InstanceOf<IndexOutOfRangeException>());
+            ;
+        }
+    }
+}
