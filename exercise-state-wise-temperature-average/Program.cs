e7ea22504c2e2dbe4b0b23cae833c6f902f50b22
Roshan Lal <153064@ust-global.com>
2020-09-01 16:03:47 +0530

    Initial commit

--- exercise-state-wise-temperature-average/Program.cs
+++ exercise-state-wise-temperature-average/Program.cs
@@ -12,23 +12,36 @@ namespace exercise_state_wise_temperature_average
              *second state provides temperature of 7 cities
              *third state provides temperature of 4 cities
             */
-
+            int[][] states = new int[3][];
+            states[0] = new int[3] {4,6,8};
+            states[1] = new int[7] {5,7,8,1,2,9,6};
+            states[2] = new int[4] {5,8,3,4};
             /*
              * Call GetStateWiseAverage() with state position and temperature data
              */
-
+            int state1Avg = GetStateWiseAverage(0,states);
+            int state2Avg = GetStateWiseAverage(1,states);
+            int state3Avg = GetStateWiseAverage(2,states);
             /*
              * Display Average
              */
+             Console.WriteLine("State 1 Average: " + state1Avg);
+             Console.WriteLine("State 2 Average: " + state2Avg);
+             Console.WriteLine("State 3 Average: " + state3Avg);
         }
 
         /* 
          * Method calculates average of temperature for the state whose position is provided
          */
 
-        public static int GetStateWiseAverage()
+        public static int GetStateWiseAverage(int index, int[][] states)
         {
             // put here logic to calculate average
+            int total = 0;
+            foreach(int temp in states[index]){
+                total += temp;
+            }
+            return total/states[index].Length;
         }
     }
 }
